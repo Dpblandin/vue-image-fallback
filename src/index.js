fb186c77559175ae1831f445180836d7cf9c8dab
David Pascaud-Blandin <dav.pascaud@gmail.com>
2018-06-09 07:55:23 +0200

    Add directive

---
+++ src/index.js
@@ -0,0 +1,68 @@
+function onError(el, options) {
+    const src = el.getAttribute('src');
+
+    if (options.loading && options.loading.length) {
+        el.setAttribute('src', options.loading);
+    }
+
+    const index = options.images.indexOf(src);
+
+    if (index === -1) {
+        const fallbackSrc = options.images[0];
+        if (fallbackSrc && src !== fallbackSrc) {
+            return el.setAttribute('src', fallbackSrc);
+        }
+    }
+
+    if (index + 1 < options.images.length) {
+        if (! options.images[index + 1]) {
+            return;
+        }
+
+        return el.setAttribute('src', options.images[index + 1]);
+    }
+
+    return null;
+}
+
+function parseBindingValue(bindingValue) {
+    if (Array.isArray(bindingValue)) {
+        return {
+            images: bindingValue,
+            loading: null,
+        }
+    }
+
+    if (typeof bindingValue === 'object') {
+        return handleObject(bindingValue);
+    }
+
+    return {
+        images: [bindingValue],
+        loading: null,
+    }
+}
+
+function handleObject(value) {
+    if (! value.hasOwnProperty('images')) {
+        return console.warn('When using object as image fallback options, you need to specify an images property')
+    }
+
+    return {
+        images: value.images,
+        loading: value.loading || null,
+    }
+}
+
+export default (Vue) => {
+    Vue.directive('imageFallback', {
+        bind(el, binding) {
+            el.addEventListener('error', onError.bind(this, el, parseBindingValue(binding.value)));
+        },
+
+        unbind(el) {
+            el.removeEventListener('error', onError);
+        },
+    });
+}
+
